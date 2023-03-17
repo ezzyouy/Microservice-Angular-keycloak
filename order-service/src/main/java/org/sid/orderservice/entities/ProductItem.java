fdd21f52c5d4d6f9b0d3546418f93b3f8eabe852
brahimEzzyouy <b.ezzyouy@etu.enset-media.ac.ma>
2023-03-17 16:58:42 +0100

    order microservice

---
+++ order-service/src/main/java/org/sid/orderservice/entities/ProductItem.java
@@ -0,0 +1,31 @@
+package org.sid.orderservice.entities;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.sid.orderservice.model.Product;
+
+import javax.persistence.*;
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor @Builder
+public class ProductItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long productId;
+    @ManyToOne
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Order order;
+    private int quantity;
+    private double price;
+    private double discount;
+    @Transient
+    private Product product;
+
+    public double getAmount(){
+        return price*quantity*(1-discount);
+    }
+
+}
---
+++ order-service/src/main/java/org/sid/orderservice/web/OrderRestController.java
@@ -0,0 +1,40 @@
+package org.sid.orderservice.web;
+
+import org.sid.orderservice.entities.Order;
+import org.sid.orderservice.model.Customer;
+import org.sid.orderservice.model.Product;
+import org.sid.orderservice.repositories.OrderRepository;
+import org.sid.orderservice.repositories.ProductItemRepository;
+import org.sid.orderservice.services.CustomerRestCient;
+import org.sid.orderservice.services.ProductRestClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderRestController {
+
+    private OrderRepository orderRepository;
+    private ProductItemRepository productItemRepository;
+    private CustomerRestCient customerRestCient;
+    private ProductRestClient productRestClient;
+
+    public OrderRestController(OrderRepository orderRepository, ProductItemRepository productItemRepository, CustomerRestCient customerRestCient, ProductRestClient productRestClient) {
+        this.orderRepository = orderRepository;
+        this.productItemRepository = productItemRepository;
+        this.customerRestCient = customerRestCient;
+        this.productRestClient = productRestClient;
+    }
+
+    @GetMapping(path = "/fullOrder/{id}")
+    public Order getOrder(@PathVariable Long id){
+        Order order=orderRepository.findById(id).get();
+        Customer customer=customerRestCient.customerById(order.getCustomerId());
+        order.setCustomer(customer);
+        order.getProductItems().forEach(pi->{
+            Product product= productRestClient.productById(pi.getProductId());
+            pi.setProduct(product);
+        });
+        return order;
+    };
+}
